754660a401152c8612746c7245a855f72939f7fb
ariskoutris <koutrisaris@gmail.com>
2024-03-23 23:05:05 +0100

    Use correct cluster name

--- part2/delete_cluster_B.sh
+++ part2/delete_cluster_B.sh
@@ -3,5 +3,5 @@
 export KOPS_STATE_STORE=gs://cca-eth-2024-group-018-akoutris/
 
 echo "Deleting the cluster..."
-kops delete cluster part2a.k8s.local --yes
+kops delete cluster part2b.k8s.local --yes
 echo "Cluster deleted."
