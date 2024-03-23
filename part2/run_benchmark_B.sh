5388d581ff879bc493bdb1eb469b9900d79d353a
ariskoutris <koutrisaris@gmail.com>
2024-03-23 22:00:27 +0100

    Upload scripts for part 2b

---
+++ part2/run_benchmark_B.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PARSEC_JOBS=("parsec-blackscholes" "parsec-canneal" "parsec-dedup" "parsec-ferret" "parsec-freqmine" "parsec-radix" "parsec-vips")
+
+for PARSEC_JOB in "${PARSEC_JOBS[@]}"; do
+    
+    TEMPLATE_FILE="parsec-benchmarks/part2b/${PARSEC_JOB}.yaml"
+    for n_threads in 1 2 4 8; do
+    
+        export N_THREADS=$n_threads
+        
+        echo "Deploying job $PARSEC_JOB with $N_THREADS threads..."
+        envsubst < $TEMPLATE_FILE | kubectl create -f -
+        
+        echo "Waiting for job to complete..."
+        while true; do
+            JOB_CONDITION=$(kubectl get jobs $PARSEC_JOB -o jsonpath='{.status.conditions[0].type}')   
+            if [ "$JOB_CONDITION" == "Complete" ]; then
+                echo "Job $PARSEC_JOB has completed successfully."
+                break
+            elif [ "$JOB_CONDITION" == "Failed" ]; then
+                echo "Job $PARSEC_JOB has failed."
+                break
+            else
+                sleep 5
+            fi
+        done
+        
+        kubectl get jobs
+        (kubectl logs $(kubectl get pods --selector=job-name=$PARSEC_JOB --output=jsonpath='{.items[*].metadata.name}')) > results/b/${PARSEC_JOB}_${n_threads}.txt
+
+        echo "Removing job $PARSEC_JOB..."
+        kubectl delete job $PARSEC_JOB
+        
+        echo "----------------------------------------"
+    done
+done
