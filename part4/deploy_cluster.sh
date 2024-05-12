b72c391c3f5227e44dd2fff063799db5c2c163ad
Georgios Manos <gmanos@student.ethz.ch>
2024-05-12 17:20:54 +0200

    added setup files for part4

---
+++ part4/deploy_cluster.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+export KOPS_STATE_STORE=gs://cca-eth-2024-group-018-mtsanta/
+CLUSTER_NAME="part4.k8s.local"
+PROJECT=`gcloud config get-value project`
+
+echo "Creating a new cluster in project ${PROJECT}..."
+kops create -f part4.yaml
+
+# Update the cluster and apply the changes
+echo "Updating and applying changes to the cluster..."
+kops update cluster --name part4.k8s.local --yes --admin
+
+# Check the exit status of the kops update cluster command
+if [ $? -ne 0 ]; then
+    echo "Cluster update failed. Exiting..."
+    exit 1
+fi
+
+# Validate the cluster
+# This command will retry for up to 10 minutes to validate the cluster
+echo "Validating the cluster, this might take up to 10 minutes..."
+kops validate cluster --wait 10m
+
+# Check the exit status of the kops validate cluster command
+if [ $? -eq 0 ]; then
+    echo "Cluster validation successful. Retrieving node information..."
+    kubectl get nodes -o wide
+else
+    echo "Cluster validation failed. Please check the cluster status and try again."
+    exit 1
+fi
---
+++ part4/setup_memcached.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# VM Names
+SERVER_VM=("$(kubectl get nodes -o wide | awk '/memcache-server/ {print $1}')")
+AGENT_VM=("$(kubectl get nodes -o wide | awk '/client-agent/ {print $1}')")
+CLIENT_VM=("$(kubectl get nodes -o wide | awk '/client-measure/ {print $1}')")
+
+# Server Configuration
+CONFIG_FILE="/etc/memcached.conf"
+SERVER_MEMORY=1024
+SERVER_THREADS=16
+SERVER_IP=("$(kubectl get nodes -o wide | awk '/memcache-server/ {print $6}')")
+SERVER_SETUP="
+    sudo apt-get update
+    sudo apt-get purge -y memcached libmemcached-tools && sudo apt-get install -y memcached libmemcached-tools
+    sudo systemctl status memcached
+    sudo sed -i '/^-m /c\-m $SERVER_MEMORY' $CONFIG_FILE
+    sudo sed -i '/^-l /c\-l $SERVER_IP' $CONFIG_FILE
+    echo '-t $SERVER_THREADS' | sudo tee -a $CONFIG_FILE > /dev/null
+    sudo systemctl restart memcached
+    sleep 1
+    sudo systemctl status memcached
+"
+# VM setup
+MCPERF_SETUP="
+    sudo sh -c 'echo deb-src http://europe-west3.gce.archive.ubuntu.com/ubuntu/ jammy main restricted >> /etc/apt/sources.list'
+    sudo apt-get update
+    sudo apt-get install libevent-dev libzmq3-dev git make g++ --yes
+    sudo apt-get build-dep memcached --yes
+    git clone https://github.com/eth-easl/memcache-perf-dynamic.git
+    cd memcache-perf-dynamic
+    make
+"
+
+echo "Setting up memcached server on $SERVER_VM..."
+gcloud compute ssh --ssh-key-file ~/.ssh/cloud-computing ubuntu@"$SERVER_VM" --zone europe-west3-a --command "$SERVER_SETUP"
+echo "Please make sure that the server is running here, or cancel the script if it is not."
+sleep 5
+echo "Setting up mcperf on $AGENT_VM..."
+gcloud compute ssh --ssh-key-file ~/.ssh/cloud-computing ubuntu@"$AGENT_VM" --zone europe-west3-a --command "$MCPERF_SETUP"
+echo "Setting up mcperf on $CLIENT_VM..."
+gcloud compute ssh --ssh-key-file ~/.ssh/cloud-computing ubuntu@"$CLIENT_VM" --zone europe-west3-a --command "$MCPERF_SETUP"
+
+echo "Setup complete."
