12fe2891d9f798452ded64e2e526ffb4f4c82182
ariskoutris <koutrisaris@gmail.com>
2024-05-17 12:48:31 +0200

    Fix docker setup

--- part4/setup_memcached.sh
+++ part4/setup_memcached.sh
@@ -22,7 +22,13 @@ SERVER_SETUP="
     sudo systemctl status memcached
 "
 
+# Adapted from https://docs.docker.com/engine/install/ubuntu/
 DOCKER_SETUP="
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
     sudo apt-get update
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     sudo systemctl start docker
@@ -55,7 +61,7 @@ MCPERF_SETUP="
 
 echo "Setting up docker on $SERVER_VM..."
 gcloud compute ssh --ssh-key-file ~/.ssh/cloud-computing ubuntu@"$SERVER_VM" --zone europe-west3-a --command "$DOCKER_SETUP"
-echo "Setting up docker on $SERVER_VM..."
+echo "Setting up python on $SERVER_VM..."
 gcloud compute ssh --ssh-key-file ~/.ssh/cloud-computing ubuntu@"$SERVER_VM" --zone europe-west3-a --command "$PYTHON_SETUP"
 echo "Setting up memcached server on $SERVER_VM..."
 gcloud compute ssh --ssh-key-file ~/.ssh/cloud-computing ubuntu@"$SERVER_VM" --zone europe-west3-a --command "$SERVER_SETUP"
